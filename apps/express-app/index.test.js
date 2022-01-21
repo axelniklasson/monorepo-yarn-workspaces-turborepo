be2a204bd701196b9ee4e2caab30a1eb784f2e0f
Axel Niklasson <axel.r.niklasson@gmail.com>
2022-01-21 13:36:15 +0100

    Make a test fail

--- apps/express-app/index.test.js
+++ apps/express-app/index.test.js
@@ -5,6 +5,6 @@ describe('endpoints', () => {
   test('should serve data on index route', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual({ ok: 'true', name: 'express-app', version: '1.0.0' });
+    expect(res.body).toEqual({ ok: 'true', name: 'express-app', version: '1.1.0' });
   });
 });
