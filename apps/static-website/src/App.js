5453f6f1610fc1638cc20c46499d7e415b24948e
Axel Niklasson <axel.r.niklasson@gmail.com>
2022-01-21 13:21:03 +0100

    Add turborepo support

--- apps/static-website/src/App.js
+++ apps/static-website/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import pack from '../package.json';
 
 function App() {
   return (
@@ -7,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello from {pack.name}@{pack.version}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
