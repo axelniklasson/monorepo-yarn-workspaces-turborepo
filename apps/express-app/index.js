f9d7dce8f20f6c6bea9039b477e246fa3de5f726
Axel Niklasson <axel.r.niklasson@gmail.com>
2022-01-21 16:33:10 +0100

    Test

--- apps/express-app/index.js
+++ apps/express-app/index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 logger.DEBUG(`${name} starting`);
 
 app.get('/', (_, res) => {
+  logger.FOO(`${name} running in env ${env}`);
   res.json({ ok: true, name, version });
 });
 
